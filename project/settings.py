7d1c60ba0448ed7ccaf566c8b32be2e088d82c09
BenardBett <kiplangatbett5@gmail.com>
2023-07-13 12:03:01 +0300

    configure my project for deployment

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-b*@@k*eanlru%u2u@xjpu0y)o(x_%-=+h6u+uuh#zg9+0o4=6v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'ecommerce',
+        'USER':'postgres',
+        'PASSWORD':'1234',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
@@ -125,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build','static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
